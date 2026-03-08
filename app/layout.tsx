c7d32cfcd17e0e1a07df1c2a85ea3e7e20bfe860
Davcid1572 <ifejiofordavid1572@gmail.com>
2026-03-08 11:57:13 +0100

    scaffolding project structure

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const font = Inter({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "David Portfolio App",
+  description:
+    "A portfolio app built with Next.js, TypeScript, Tailwind CSS, and Shadcn UI.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${font.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
