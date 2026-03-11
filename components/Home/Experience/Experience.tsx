19e303f896dd3ccde87df1fcae99b505a89121a0
Davcid1572 <ifejiofordavid1572@gmail.com>
2026-03-11 21:00:54 +0100

    added the experience section

--- components/Home/Experience/Experience.tsx
+++ components/Home/Experience/Experience.tsx
@@ -1,5 +1,6 @@
 import SectionHeading from "@/components/Helper/SectionHeading";
 import { experiences } from "@/data";
+import { Briefcase, GraduationCap } from "lucide-react";
 import React from "react";
 
 const Experience = () => {
@@ -14,8 +15,52 @@ const Experience = () => {
         {/* Timeline Line */}
         <div className="absolute left-4 md:left-1/2 top-0 bottom-0 w-px bg-linear-to-b from-blue-400 to to-blue-900 md:-translate-x-px"></div>
         {/* Timeline Items */}
-        {experiences.map((exp) => {
-          return <div></div>;
+        {experiences.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className={`relative flex flex-col md:flex-row gap-8 mb-12  ${index % 2 === 0 ? "md:flex-row-reverse" : ""}`}
+            >
+              {/* time node */}
+              <div className="absolute left-4 md:left-1/2 w-8 h-8 -translate-x-1/2 rounded-full bg-white border-2 border-blue-500 flex justify-center items-center z-10">
+                {item.type === "work" ? (
+                  <Briefcase className="w-4 h-4 text-blue-500" />
+                ) : (
+                  <GraduationCap className="w-4 h-4 text-blue-500" />
+                )}
+              </div>
+              {/* content */}
+              <div className="ml-16 md:ml-0 md:w-[calc(50%-2rem)]">
+                <div className="bg-white dark:bg-gray-900 shadow-md round-xl p-6 hover:scale-105 transition-all  duration-300">
+                  <div className="flex items-center gap-2 text-sm text-blue-500 mb-2">
+                    <span className="px-3 py-1 rounded-full bg-blue-600/10 font-medium">
+                      {item.period}
+                    </span>
+                  </div>
+                  <h3 className="text-xl font-semibold mb-1">{item.title}</h3>
+                  <p className="text-muted-foreground text-sm mb-3">
+                    {item.company}
+                  </p>
+                  <p className="text-muted-foreground text-sm mb-4">
+                    {item.description}
+                  </p>
+                  <div className="flex flex-wrap gap-2">
+                    {item.technologies.map((tech) => {
+                      return (
+                        <span
+                          key={tech}
+                          className="text-xs px-2 py-1 rounded-md bg-indigo-600 text-white"
+                        >
+                          {tech}
+                        </span>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+              <div className="hidden md:block md:w-[calc(50%-2rem)}"></div>
+            </div>
+          );
         })}
       </div>
     </div>
